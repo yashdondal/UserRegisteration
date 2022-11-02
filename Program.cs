6357ce9510eb7cd07eeffcb258db5263b08a9e62
yashdondal <yashdondal21@gmail.com>
2022-11-02 18:33:35 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UserRegistrationForm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidationForm form = new ValidationForm();
+            Console.WriteLine("Enter The First Name: ");
+            string firstName = Console.ReadLine();
+            form.FirstName(firstName);
+        }
+    }
+}
