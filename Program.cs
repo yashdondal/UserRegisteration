c2c9f1cbcc4b5bb7136e7f9bdba95ad1979502dd
yashdondal <yashdondal21@gmail.com>
2022-11-02 18:49:37 +0530

    enter mail

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@
             Console.WriteLine("Enter The First Name: ");
             string firstName = Console.ReadLine();
             form.FirstName(firstName);
+            Console.WriteLine("Enter The Last Name: ");
+            string lastName = Console.ReadLine();
+            form.FirstName(lastName);
+            Console.WriteLine("Enter valid email: ");
+            string emaiId = Console.ReadLine();
+            form.EmailID(emaiId);
         }
     }
 }
--- Validation.cs
+++ Validation.cs
@@ -13,6 +13,8 @@ namespace UserRegistrationForm
         public const string FIRST_NAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2}$";
         //UC2......
         public const string LAST_NAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2}$";
+        //UC3
+        public const string EMAILID_REGEX = "^[a-z]+[.][a-z]+[@][a-z]+[.][a-z]+[.][a-z]{2,3}$";
         public void FirstName(string firstname)
         {
             Regex regex = new Regex(FIRST_NAME_REGEX);
@@ -23,6 +25,12 @@ namespace UserRegistrationForm
         {
             Regex regex = new Regex(LAST_NAME_REGEX);
             bool result = regex.IsMatch(lastname);
+            Console.WriteLine(result);
+        }
+        public void EmailID(string emailid)
+        {
+            Regex regex = new Regex(EMAILID_REGEX);
+            bool result = regex.IsMatch(emailid);
             Console.WriteLine(result);
         }
     }
