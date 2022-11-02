bd6b4bdca5c14eabcd23f1b28e089219aeddbe57
yashdondal <yashdondal21@gmail.com>
2022-11-02 18:36:13 +0530

    added uc2

--- Validation.cs
+++ Validation.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace UserValidationForm
+namespace UserRegistrationForm
 {
-    internal class Validation
+    internal class ValidationForm
     {
+        //UC1
+        public const string FIRST_NAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2}$";
+        //UC2......
+        public const string LAST_NAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2}$";
+        public void FirstName(string firstname)
+        {
+            Regex regex = new Regex(FIRST_NAME_REGEX);
+            bool result = regex.IsMatch(firstname);
+            Console.WriteLine(result);
+        }
+        public void LastName(string lastname)
+        {
+            Regex regex = new Regex(LAST_NAME_REGEX);
+            bool result = regex.IsMatch(lastname);
+            Console.WriteLine(result);
+        }
     }
 }
